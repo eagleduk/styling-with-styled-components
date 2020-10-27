0ed45318eff7a2dc2c1a36fa8173671313e7937e
eagleduk <eagleduk@gmail.com>
2020-10-27 22:07:14 +0900

    2. 재사용성

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 - yarn add styled-components
 - css style을 함수형태로 js 파일에서 선언가능
 - css 가 포함된 component 같이 동작한다.
+
+2. 재사용성
+- styled-components 의 ThemeProvider 을 사용하여 원하는 theme 을 component 에 부여할 수 있다. 마치 전역변수처럼
+- styled-components 를 선언할때에는 props 로 변수를 받을 수 있고, 받은 변수로 함수를 만들어서 사용도 가능하다.
+** 다양한 색상 함수를 사용하기 위한 라이브러리(https://polished.js.org/docs/) - yarn add polished
--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 
-import styled, { css } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "./components/Buttton";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${props => props.color};
-  border-radius: 50%;
-  ${props => props.huge && 
-    css `
-      width: 10rem;
-      height: 10rem;
-    `}
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 
+const palette = {
+  blue: "#228be6",
+  gray: "#496057",
+  pink: "#f06595"
+}
 
 function App() {
-  return (<Circle color="blue" huge/>
+  return (
+    <ThemeProvider theme={{palette}}>
+      <AppBlock>
+        <Button>BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
